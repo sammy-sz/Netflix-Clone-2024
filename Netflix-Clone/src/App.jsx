8648bc0eb6f972dd8d73c013c9012a133ad878f7
samiz1971 <samsonzewdie@yahoo.com>
2025-02-09 03:33:59 +0000

    header and footer completed

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Netflix</h1>
+      <div className='App'>
+        <h1>
+          <Home />
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
---
+++ Netflix-Clone/src/Components/Footer/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./footer.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+const Footer = () => {
+  return (
+    <div className="footer_outer_container">
+      <div className="footer_inner-container">
+        <div className="footer_icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+          <TwitterIcon />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy_write">&copy; 1997-2025 Netflix, Inc.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
