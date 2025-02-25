b26df1f8fdb0fbe9d17b4c1f41db7ee9bdb91ccb
samiz1971 <samsonzewdie@yahoo.com>
2025-02-25 19:02:59 +0000

    Row modified

--- Netflix-Clone/src/Components/Rows/RowsList/RowList.jsx
+++ Netflix-Clone/src/Components/Rows/RowsList/RowList.jsx
@@ -18,7 +18,7 @@ const RowList = () => {
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="TV Shows" fetchUrl={requests.fetchTvShow} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Animation" fetchUrl={requests.fetchAnimation} />
     </>
   );
 }
