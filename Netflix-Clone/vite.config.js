51d499667560754a40fc9b3cbafaf6bc1ff7361c
Samson Zewdie Samuel <35701796+sammy-sz@users.noreply.github.com>
2025-05-23 03:01:18 +0100

    Update vite.config.js

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -5,8 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Netflix-Clone-2024/",
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
 });
